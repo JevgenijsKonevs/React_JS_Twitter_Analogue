91c5bd92f316ec1da7337fb67ed7e6b4b161fac0
nbx <j.konev@mail.ru>
2020-05-03 00:28:37 +0300

    Initial commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./app-header.css";
+import styled from 'styled-components';
+
+const Header = styled.div`
+display:flex;
+align-item:flex-end;
+justify-content: space-between;
+h1 {
+    font-size: 26px;
+    color: ${props => props.colored ? 'red' : 'black'}
+    :hover {
+        color:blue;
+    }
+}
+h2 {
+    font-size: 1.2rem;
+    color:grey;
+}
+`
+const AppHeader = ({liked,allPosts}) => {
+    return (
+            <Header colored>
+                <h1>Name Surname</h1>
+    <h2>{allPosts} records, from them liked {liked}</h2>
+            </Header>
+    )
+}
+export default AppHeader;
